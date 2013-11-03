4f2383968d855c30ce1345ff3976434c5b4c4a8b
Hobey Kuhn <hkuhn@umich.edu>
2013-11-03 16:10:34 +0000

    began writing code for summary generation component of system. Only thing that has been added are comments.

---
+++ summ_generation.py
@@ -0,0 +1,23 @@
+# Title:
+#	Summary Generation
+#
+# Source:
+#	Query-Oriented Multi-Document Summarization via Unsupervised Deep Learning
+#	Yan Liu, Sheng-hua Zhong, Wenjie Li
+#	Department of Computing, The Hong Kong Polytechnic University
+#	AAAI, 2012
+#
+# Description: 
+#	This class is the final component to Liu's summarization system.
+#	It builds an importance matrix (AF) from values fed from a 
+#	deep neural network.  It then extracts 10 word with largest
+#	AF value i every nth node and uses these to calculate the
+#	importance of every sentence in the document. Dynamic
+#	programming is then used to solve the optimization problem
+#	of determining which sentences should be placed in the summary.
+#
+#
+#
+
+
+
