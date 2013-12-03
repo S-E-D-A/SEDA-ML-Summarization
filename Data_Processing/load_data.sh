d7415775f8e615a875c59f858b3ef3cf6e108be1
vagrant <vagrant@precise64.(none)>
2013-12-03 21:51:08 +0000

    updated load_data.sh to be more generalized. forces two command line inputs, one to specify the name of the dataset and the second to specify the core name

--- Data_Processing/load_data.sh
+++ Data_Processing/load_data.sh
@@ -6,34 +6,19 @@
 #  Created by Hobey Kuhn on 11/18/13.
 #
 
-# Enforce JSON format
-echo "Before continuing, make sure your files in xml format! Also make sure that there is no directory called solr_files if you are planning on converting files first"
-
-# read in file name
-echo "Which dataset do you wish to load? Specify: Australia / DUC"
-read dataset_name
 
 
-# test input
-flag=true
-while [ ${flag} ]; do
-    if [ ${dataset_name} != "Australia" ] && [ ${dataset_name} != "DUC" ]; then
-        echo "Please enter either Australia or DUC"
-        read dataset_name
-    else
-        break
-    fi
-done
+
 
 # configure path to json files
 # NOTE: DUC PATH NOT CONFIGURED
-if [ ${dataset_name} = "Australia" ]; then
+if [ ${1} = "Australia" ]; then
     # Obtains and unzips the Australian dataset if not alraedy present
     sh getAustralianDataset.sh
     data_path="./corpus/fulltext/*"
 
 else
-    echo "DUC set is unavailable at the moment!"
+    echo "Invalid input in command line. Please read the README to learn how to use this script!"
     exit
 fi
 
@@ -47,7 +32,7 @@ else
     for f in ${data_path}
     do
         filename=$(basename $f)
-    if [ "${dataset_name}" = "Australia" ]; then
+    if [ "${1}" = "Australia" ]; then
         python australia_xml_parser.py "${f}" "solr_files/${filename}"
     fi
     done
@@ -55,5 +40,5 @@ fi
 # PUT data from path onto SOLR
 echo "Posting files to SOLR"
 cp -a "solr_config" "/home/vagrant"
-curl "http://localhost:8983/solr/admin/cores?action=CREATE&name=AustralianDataset&instanceDir=/home/vagrant/solr_config/collection1"
-java -Durl=http://localhost:8983/solr/AustralianDataset/update -jar post.jar "solr_files/*.xml"
+curl "http://localhost:8983/solr/admin/cores?action=CREATE&name=${2}&instanceDir=/home/vagrant/solr_config/collection1"
+java -Durl=http://localhost:8983/solr/${2}/update -jar post.jar "solr_files/*.xml"
