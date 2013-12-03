49c7c8a73dc849fe2bf59e4642a425d937db9673
vagrant <vagrant@precise64.(none)>
2013-12-03 22:18:10 +0000

    Modified download_rbm_term_frequncies to use data folder and variables

--- Data_Processing/download_australian_rbm_input.sh
+++ Data_Processing/download_australian_rbm_input.sh
@@ -1,16 +1,9 @@
-curl "http://localhost:8983/solr/collection1/tvrh?q=*%3A*&wt=python&indent=true&tv.fl=features&tv.all=true&fl=false&start=0&rows=300" > australian_batch_1
-curl "http://localhost:8983/solr/collection1/tvrh?q=*%3A*&wt=python&indent=true&tv.fl=features&tv.all=true&fl=false&start=300&rows=300" > australian_batch_2
-curl "http://localhost:8983/solr/collection1/tvrh?q=*%3A*&wt=python&indent=true&tv.fl=features&tv.all=true&fl=false&start=600&rows=300" > australian_batch_3
-curl "http://localhost:8983/solr/collection1/tvrh?q=*%3A*&wt=python&indent=true&tv.fl=features&tv.all=true&fl=false&start=900&rows=300" > australian_batch_4
-curl "http://localhost:8983/solr/collection1/tvrh?q=*%3A*&wt=python&indent=true&tv.fl=features&tv.all=true&fl=false&start=1200&rows=300" > australian_batch_5
-curl "http://localhost:8983/solr/collection1/tvrh?q=*%3A*&wt=python&indent=true&tv.fl=features&tv.all=true&fl=false&start=1500&rows=300" > australian_batch_6
-curl "http://localhost:8983/solr/collection1/tvrh?q=*%3A*&wt=python&indent=true&tv.fl=features&tv.all=true&fl=false&start=1800&rows=300" > australian_batch_7
-curl "http://localhost:8983/solr/collection1/tvrh?q=*%3A*&wt=python&indent=true&tv.fl=features&tv.all=true&fl=false&start=2100&rows=300" > australian_batch_8
-curl "http://localhost:8983/solr/collection1/tvrh?q=*%3A*&wt=python&indent=true&tv.fl=features&tv.all=true&fl=false&start=2400&rows=300" > australian_batch_9
-curl "http://localhost:8983/solr/collection1/tvrh?q=*%3A*&wt=python&indent=true&tv.fl=features&tv.all=true&fl=false&start=2700&rows=300" > australian_batch_10
-curl "http://localhost:8983/solr/collection1/tvrh?q=*%3A*&wt=python&indent=true&tv.fl=features&tv.all=true&fl=false&start=3000&rows=300" > australian_batch_11
-curl "http://localhost:8983/solr/collection1/tvrh?q=*%3A*&wt=python&indent=true&tv.fl=features&tv.all=true&fl=false&start=3300&rows=300" > australian_batch_12
-curl "http://localhost:8983/solr/collection1/tvrh?q=*%3A*&wt=python&indent=true&tv.fl=features&tv.all=true&fl=false&start=3600&rows=300" > australian_batch_13
+BASE_URL="http://localhost:8983/solr"
+CORE="AustralianDataset"
+QUERY="tvrh?q=*%3A*&wt=python&indent=true&tv.fl=features&tv.all=true&fl=false&start=0&rows=999999999"
+FINAL_QUERY="${BASE_URL}/${CORE}/${QUERY}"
 
+echo ${FINAL_QUERY}
+echo "Writing Ausralian legal dataset term frequencies" 
 
-
+curl "${FINAL_QUERY}" > "../data/australianLegalDataset_term_frequencies"
