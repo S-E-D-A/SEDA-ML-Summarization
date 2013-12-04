1097ca46544206b10badc7a3808285d0835ad1fb
vagrant <vagrant@precise64.(none)>
2013-12-04 21:18:38 +0000

    Modified download_rbm_input sh script to reference relative to itself

--- Data_Processing/download_australian_rbm_input.sh
+++ Data_Processing/download_australian_rbm_input.sh
@@ -6,4 +6,8 @@ FINAL_QUERY="${BASE_URL}/${CORE}/${QUERY}"
 echo ${FINAL_QUERY}
 echo "Writing Ausralian legal dataset term frequencies" 
 
-curl "${FINAL_QUERY}" > "../data/australianLegalDataset_term_frequencies"
+# Get the directory of this script to use in reference to
+SCRIPT_PATH=$( cd $(dirname $0) ; pwd -P )
+DATASET_DIRECTORY="${SCRIPT_PATH}/../data"
+
+curl "${FINAL_QUERY}" > "${DATASET_DIRECTORY}/australianLegalDataset_term_frequencies"
