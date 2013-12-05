0a7ab1c585a9d2b681b7ec284f96cf41457851c8
vagrant <vagrant@precise64.(none)>
2013-12-05 19:38:22 +0000

    adding empty files for summary generation initializing

---
+++ Summary_Generation/build_vocabulary.py
@@ -0,0 +1,13 @@
+#
+# build_vocabulary.py
+#
+#
+# Builds a vocabulary given path to word list of document set
+#    Then stores the vocabulary as a numpy file that can be read in later during summary generation
+
+
+
+
+
+
+
---
+++ Summary_Generation/parse_document_into_sentences.py
@@ -0,0 +1,18 @@
+# TITLE
+#    Parse Document into Sentences
+#
+# DESCRIPTION
+#    Given an input file (basename), parse xml sentences into numpy sentences
+#    Store the numpy array in a file called document_senteces.npy
+#    Numpy array should be an array of sentences of words
+#
+#    NOTE: Be sure that the solr_xml_files directory corresponds to the current document set!
+#
+# EXAMPLE
+#    python parse_document_into_sentences.py 06_001.xml
+
+
+import sys
+
+
+
