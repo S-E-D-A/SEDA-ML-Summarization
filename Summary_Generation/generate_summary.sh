60fe5d5e99ab91efa7cc7fe773a3560bf581a6da
vagrant <vagrant@precise64.(none)>
2013-12-05 19:17:31 +0000

    summary generation file updated. I need to know what query to run to obtain the vocabulary though

--- Summary_Generation/generate_summary.sh
+++ Summary_Generation/generate_summary.sh
@@ -9,11 +9,16 @@
 # Description:
 #    Generates a summary with name of dataset and filename as input.
 
+
+
 DATASET_NAME="${1}"
 DOCUMENT_NAME="${2}"
 DATA_DIR="../data"
 AUSTRALIAN_DATASET_DIR="${DATA_DIR}/corpus/"
 AUSTRALIAN_DATASET_LABELS_DIR="${DATA_DIR}/australian_dataset_labels"
+
+DICTIONARY_DATASET_DIR="${DATA_DIR}/dictionary_data"
+
 
 
 # confirm dataset name and retrieve labels
@@ -29,6 +34,17 @@ if [ -f "${dataset_labels_path}/${DOCUMENT_NAME}" ]; then
 else
    echo "File specified was not found! Please make sure generate_labels.sh was run before this to ensure your labels have been generated"
 fi
+
+
+# generate vocabulary for summary generation
+#    first, run query to obtain vocabulary
+#    then, run build_vocabulary.py to create numpy file
+if [ -d "${DICTIONARY_DATASET_DIR}" ]; then
+    echo "dictionary data already exists. Skipping query from SOLR to retrieve dictionary data..."
+else
+    # curl a query
+fi
+
 
 
 # parse labels into numpy lists (sentences of words)
