7c36a4b72c29d190480e25a44b2d1cfc7e4d2b46
vagrant <philipp.cannons@gmail.com>
2013-12-18 08:15:32 +0000

    runEvaluation takes one argument and stores the ROUGE scores in SEDA data in that argument's name folder

---
+++ Evaluation/runEvaluation.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+if [ ! -z ${SEDA_DEBUG_MODE} ]
+then
+	set -ex
+fi
+
+# Get the directory of this script to use in reference to
+SCRIPT_PATH=$( cd $(dirname $0) ; pwd -P )
+
+# Install ROUGE
+sh "${SCRIPT_PATH}/ROUGE/installRouge.sh"
+
+# Store ROGUE installation directory in variable
+ROUGE_DIRECTORY="${SCRIPT_PATH}/ROUGE/RELEASE-1.5.5"
+
+# Set variable for ROUGE executable
+ROUGE="${ROUGE_DIRECTORY}/ROUGE-1.5.5.pl"
+
+# Stroe the generate.py script into a variable
+GENERATE_PY="${SCRIPT_PATH}/generation_scripts/generate.py"
+
+# If given an argument output everything to there,
+# if not, use EvaluationOutput in the data folder by default
+if [ -z $1 ]
+then
+	eval "${GENERATE_PY}" 
+	OUTPUT_DIR="${SCRIPT_PATH}/../data/EvaluationOutput"
+else
+	eval "${GENERATE_PY}" "${1}"
+	OUTPUT_DIR="${SCRIPT_PATH}/../data/${1}"
+fi
+
+eval "${ROUGE}" -e "${ROUGE_DIRECTORY}/data" \
+	-f A -a -x -s -m -2 -4 -u "${OUTPUT_DIR}/rougeFiles/settings.xml" > \
+	"${OUTPUT_DIR}/results.out"
+
