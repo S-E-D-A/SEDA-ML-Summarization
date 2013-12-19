0da1406d8e4a09d6f7b8716701400640807d53c3
vagrant <vagrant@precise64.(none)>
2013-12-19 11:07:23 +0000

    Moved ROUGE perl DOM package install outside of ROUGE install loop, as the ROUGE install is persistant beyond vagrant reloads and the perl package install is not

--- Evaluation/ROUGE/installRouge.sh
+++ Evaluation/ROUGE/installRouge.sh
@@ -7,18 +7,23 @@ ROUGE_FILE_NAME="ROUGE-1.5.5.tgz"
 ROUGE_URL="https://www.dropbox.com/s/plcw48pi0rjzkge/${ROUGE_FILE_NAME}"
 DOT_ROUGE_INSTALLED="${SCRIPT_PATH}/.rougeinstalled"
 
+# Install necessary Perl package for Rouge to functin
+perl -MCPAN -e 'force install XML::DOM'
+
 if [ ! -f "${DOT_ROUGE_INSTALLED}" ]; then
 	echo "Installing ROUGE.\n"
 
+	# Download Rouge
 	wget "${ROUGE_URL}" -O "${SCRIPT_PATH}/${ROUGE_FILE_NAME}"
 
-	perl -MCPAN -e 'force install XML::DOM'
-
+	# Unzip Rouge
 	tar -zxvf "${SCRIPT_PATH}/${ROUGE_FILE_NAME}" \
 		--directory="${SCRIPT_PATH}"
 
+	# Remove the tar file
 	rm "${SCRIPT_PATH}/${ROUGE_FILE_NAME}"
 
+	# Create file to indicate the file has been installed
 	touch "${DOT_ROUGE_INSTALLED}"
 else
 	echo "Rouge installed.\n"
