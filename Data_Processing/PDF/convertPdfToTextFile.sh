87e6ccd18719c14d61bf763697fde762f49d194c
Philipp Cannons <philipp.cannons@gmail.com>
2014-01-22 16:45:05 +0000

    Switched convertPdfToTextFile.sh to getopt format

--- Data_Processing/PDF/convertPdfToTextFile.sh
+++ Data_Processing/PDF/convertPdfToTextFile.sh
@@ -12,15 +12,34 @@ then
         set -ex
 fi
 
-# Check if there are 2 arguments, one for core name, other for text file
-if [ $# -ne 2 ]; then
-  echo "#############################################"
-  echo "Error, incorrect number of arguments supplied"
-  echo "Usage: $0 INPUT_PDF_PATH OUTPUT_PATH_NAME"
-  exit
+# The function to describe how to run the script
+USAGE_STRING="Usage: $0 [-i <input_pdf_file>] [-o <output_text_file>] [-h help]"
+usage() { echo ${USAGE_STRING} 1>&2; exit 1; }
+
+while getopts ":i:o:h" o; do
+    case "${o}" in
+        i)
+            INPUT_FILE=${OPTARG}
+            ;;
+        o)
+            OUTPUT_FILE=${OPTARG}
+            ;;
+        h)
+            usage
+            ;;
+        *)
+            usage
+            ;;
+    esac
+done
+shift $((OPTIND-1))
+
+if [ -z "${INPUT_FILE}" ] || [ -z "${OUTPUT_FILE}" ]; then
+    usage
 fi
 
 # Get the directory of this script to use in reference to
 SCRIPT_PATH=$( cd $(dirname $0) ; pwd -P )
 
-java -jar "${SCRIPT_PATH}/pdfbox-app-1.8.3.jar" ExtractText -nonSeq $1 $2
+# Perform the conversion from PDF to text
+java -jar "${SCRIPT_PATH}/pdfbox-app-1.8.3.jar" ExtractText -nonSeq "${INPUT_FILE}" "${OUTPUT_FILE}"
